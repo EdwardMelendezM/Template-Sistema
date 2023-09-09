17ba34f1cd64e1eb428190aefbb3aea1b9797e41
EdwardMelendezM <192666@unsaac.edu.pe>
2023-09-09 12:21:15 -0500

    run ng test

--- interface/src/app/app.component.spec.ts
+++ interface/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Template-sistema app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Template-sistema app is running!');
   });
 });
